60258060b75cc9b9012ba863a8c15c66e854747d
Auutof <salohitursunov@gmail.com>
2025-12-10 22:40:22 +0500

    exercise 1 solution

--- src/esp32_lab1.ino
+++ src/esp32_lab1.ino
@@ -8,9 +8,20 @@
 // light sensor - D33
 
 #include "Arduino.h"
+#define RED 15
+#define GRN 4
+#define BLU 22
+#define YLW 23
 
+#define BTN 14 //button port
+#define LSR 33 //light sensor port
+
+//exercise 1: Solution
 /****************************************************/
 void setup(void) {
+    Serial.begin(9600);
+
+    pinMode(RED, OUTPUT);
 
 }
 
@@ -18,4 +29,13 @@ void setup(void) {
 /****************************************************/
 void loop(void) {
 
+    digitalWrite(RED,HIGH);
+    Serial.println("RED ON");
+
+    delay(500);
+
+    digitalWrite(RED,LOW);
+    Serial.println("RED OFF");
+    delay(500);
+ 
 }
